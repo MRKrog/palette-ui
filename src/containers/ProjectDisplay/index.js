c5a4c6c2579b0ab96355652c24a01d263f4a0e84
Michael Krog <michaelryankrog@gmail.com>
2019-05-08 19:59:38 -0600

    Created Modal and Explore Sections

--- src/containers/ProjectDisplay/index.js
+++ src/containers/ProjectDisplay/index.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Projects from '../Projects/Projects'
+import { withStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
 
-export class ProjectDisplay extends Component {
-  constructor() {
-    super()
-    this.state = {
 
-    }
-  }
-  render(){
+const styles = {
+
+};
+
+class SwipeableTemporaryDrawer extends Component {
+  state = {
+    bottom: false,
+  };
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
     return (
       <div className="ProjectDisplay">
-        <h1>Project Display</h1>
+
+        <Button onClick={this.toggleDrawer('bottom', true)} className="ProjectBtn">
+          Explore <i className="fas fa-angle-up UpBtn"></i>
+        </Button>
+
+        <SwipeableDrawer
+          anchor="bottom"
+          open={this.state.bottom}
+          onClose={this.toggleDrawer('bottom', false)}
+          onOpen={this.toggleDrawer('bottom', true)}
+        >
+          <div className="ProjectDisplay-Content">
+            <Projects />
+          </div>
+        </SwipeableDrawer>
       </div>
-    )
+    );
   }
 }
 
+SwipeableTemporaryDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export const mapStateToProps = (state) => ({
-
-})
-
-export const mapDispatchToProps = (dispatch) => ({
-
-})
-
-ProjectDisplay.propTypes = {
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectDisplay);
+export default withStyles(styles)(SwipeableTemporaryDrawer);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 import { loadingReducer } from './loadingReducer';
 import { errorReducer } from './errorReducer';
 import { paletteReducer } from './paletteReducer';
+import { modalReducer } from './modalReducer';
 
 export const rootReducer = combineReducers({
+  modalDisplay: modalReducer,
   currentPalette: paletteReducer,
   loading: loadingReducer,
   error: errorReducer,
